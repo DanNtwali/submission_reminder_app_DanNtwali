b50c3778d7bc6d06f3fc89cc1dff6e919d7f634d
DanNtwali <danielntwali9@gmail.com>
2025-06-19 22:57:38 +0200

    feat(created the copilot to change the )

---
+++ copilot_shell_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter new assignment name:"
+read new_assignment
+
+# Update assignment name in config.env
+sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=\"$new_assignment\"/" submission_reminder_*/config/config.env
+
+echo "Assignment updated to $new_assignment"
+
+cd submission_reminder_*/
+./startup.sh
+
